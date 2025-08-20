d5f94f4d705ca2510bad06a659c78872338f9fad
lktop <my-name@chromium.org>
2025-08-20 22:46:03 +0800

    test add origin header

--- src/proto/h1/role.rs
+++ src/proto/h1/role.rs
@@ -207,10 +207,10 @@ impl Http1Transaction for Server {
             }
         };
 
-        // 保存原始报文  by lktop
-        let original_headers = OriginalHeaders::new(buf.slice(len).freeze());
-
         let slice = buf.split_to(len).freeze();
+        // 保存原始报文  by lktop
+        let original_headers = OriginalHeaders::new(slice.clone());
+
         let uri = {
             let uri_bytes = slice.slice_ref(&slice[path_range]);
             // TODO(lucab): switch to `Uri::from_shared()` once public.
